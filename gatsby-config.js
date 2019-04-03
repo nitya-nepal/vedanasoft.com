00374c9f9b46579f738208ceb4c9592f18cae04b
saileshkasaju <leosailesh@gmail.com>
2019-04-03 14:49:20 +0545

    title update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Vedana Soft',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Vedana Software and Technology solutions will take your idea to completion.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
