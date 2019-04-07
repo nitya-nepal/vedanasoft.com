95249a9d62495bd923e27ab9fa86f908c63709f4
saileshkasaju <leosailesh@gmail.com>
2019-04-07 23:04:05 +0545

    clean footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import logo from '../img/logo.jpg';
-import facebook from '../img/social/facebook.svg';
-import instagram from '../img/social/instagram.svg';
-import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
+// import facebook from '../img/social/facebook.svg';
+// import instagram from '../img/social/instagram.svg';
+// import twitter from '../img/social/twitter.svg';
+// import vimeo from '../img/social/vimeo.svg';
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <>
         <div className="content has-text-centered">
           <img src={logo} alt="VEDANA SOFT" style={{ width: '14em', height: '14em' }} />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+        <footer className="footer">
+          <div className="content has-text-centered has-background-black has-text-white-ter">
+            <p>
+              Copyright © 2019 <Link to="/">VedanaSoft</Link>. All Rights Reserved
+            </p>
+            {/* <div className="container has-background-black has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
                 <section className="menu">
@@ -69,9 +73,10 @@ const Footer = class extends React.Component {
                 </a>
               </div>
             </div>
+          </div> */}
           </div>
-        </div>
-      </footer>
+        </footer>
+      </>
     );
   }
 };
