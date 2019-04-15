8733521e627682df8728e469dcabf9dc45da3f29
Sailesh Kasaju <leosailesh@gmail.com>
2019-04-15 16:58:25 +0545

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'gatsby';
 // import github from '../img/github-icon.svg';
 import logo from '../img/logo.jpg';
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -37,8 +39,8 @@ const Navbar = class extends React.Component {
       <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="VEDANA SOFT">
-              VEDANA SOFT
+            <Link to="/" className="navbar-item" title="VEDANASOFT">
+              HOME
             </Link>
             {/* Hamburger menu */}
             <div className={`navbar-burger burger ${this.state.navBarActiveClass}`} data-target="navMenu" onClick={() => this.toggleHamburger()}>
@@ -50,19 +52,26 @@ const Navbar = class extends React.Component {
           <div id="navMenu" className={`navbar-menu ${this.state.navBarActiveClass}`}>
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about/">
-                About
+                ABOUT US
               </Link>
               <Link className="navbar-item" to="/products/">
-                Products
+                PRODUCTS
               </Link>
               <Link className="navbar-item" to="/blog/">
-                Blog
+                BLOG
               </Link>
               <Link className="navbar-item" to="/contact/">
-                Contact
+                CONTACT
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
+              <span className="navbar-item">01-4356221 vedanasoft@gmail.com</span>
+              <a className="navbar-item" title="facebook" href="https://facebook.com/vedanasoft">
+                <img src={facebook} alt="Facebook" style={{ width: '1em', height: '1em' }} />
+              </a>
+              <a className="navbar-item" title="instagram" href="https://instagram.com/vedanasoft">
+                <img src={instagram} alt="Instagram" style={{ width: '1em', height: '1em' }} />
+              </a>
               <Link to="/" title="VEDANA SOFT">
                 <img src={logo} alt="Vedana Soft" style={{ width: '88px', height: '88px' }} />
               </Link>
