7a6c69666c258a0f076c55b48fac27f3373dd932
Sailesh Kasaju <leosailesh@gmail.com>
2019-04-24 00:16:20 +0545

    host bootstrap css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,11 +46,16 @@ const TemplateWrapper = ({ children }) => {
         <meta property='og:url' content='/' />
         <meta property='og:image' content='/img/og-image.jpg' />
 
-        <link
+        {/* <link
           rel='stylesheet'
           href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
           integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u'
           crossorigin='anonymous'
+        /> */}
+
+        <link
+          rel='stylesheet'
+          href='/plugins/bootstrap/css/bootstrap.min.css'
         />
         <link rel='stylesheet' href='/plugins/Ionicons/css/ionicons.min.css' />
         <link rel='stylesheet' href='/plugins/animate-css/animate.css' />
