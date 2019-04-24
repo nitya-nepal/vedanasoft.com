592c2e667a070ceee70ad8fbc76a0a522901f15e
saileshkasaju <leosailesh@gmail.com>
2019-04-24 21:28:20 +0545

    add jquery and bootstrap js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -70,6 +70,8 @@ const TemplateWrapper = ({ children }) => {
         />
 
         <link rel='stylesheet' href='/css/style.css' />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       </Helmet>
       <Navbar />
       <>{children}</>
